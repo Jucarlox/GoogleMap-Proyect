347bc17db61922ee80b5d4817340d71a9989b9b3
ardana.mujua21 <cardamu02@gmail.com>
2021-12-21 11:33:59 +0100

    Avances

---
+++ GoogleMaps_Proyecto/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'objectnotfoundexception',
+    appId: '1:508698239477:web:f97a40ee48287a0956c0bc',
+    storageBucket: 'objectnotfoundexception.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyAOXx2y0fFfUqoyWg1bQaftMclR9NLKi2I',
+    authDomain: 'objectnotfoundexception.firebaseapp.com',
+    messagingSenderId: '508698239477',
+    measurementId: 'G-L7GEY2ZEGE',
+  },
+  production: true
+};
---
+++ GoogleMaps_Proyecto/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'objectnotfoundexception',
+    appId: '1:508698239477:web:f97a40ee48287a0956c0bc',
+    storageBucket: 'objectnotfoundexception.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyAOXx2y0fFfUqoyWg1bQaftMclR9NLKi2I',
+    authDomain: 'objectnotfoundexception.firebaseapp.com',
+    messagingSenderId: '508698239477',
+    measurementId: 'G-L7GEY2ZEGE',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
